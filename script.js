376a07e8694dee5dc1617236615ce45ad795b5e1
nikoloz93 <tsogiaidze@yahoo.com>
2019-12-13 23:22:26 +0400

    Moving arround draged element

--- script.js
+++ script.js
@@ -3,24 +3,36 @@
 	let sortableObj = {
 		selector: null,
 		init: init,
-		pressedElement: null
+		pressedElement: null,
+		clonedElement: null,
+		clonedElementContainer: null,
+		mousePosition: { x:0, y:0 }
 	};
 
 	function init(){
 		let container = document.querySelector(this.selector);
 
+		this.clonedElementContainer = document.body.appendChild(document.createElement("div"));
+		this.clonedElementContainer.setAttribute("style", "position:fixed;");
+
 		let sortableElements = (Array.from(container.childNodes)).filter((element) => { return element.nodeType == element.ELEMENT_NODE });
 
 		addMouseDownEvents.call(this, sortableElements);
 
-		setInterval(updateElementLocation.bind(this), 100);
+		document.addEventListener("mousemove", (event) => {
+			this.mousePosition = { x: event.pageX, y: event.pageY };
+		})
+
+		setInterval(updateElementLocation.bind(this), 10);
 
 		return this;
 	}
 
 	function updateElementLocation(){
 		if(this.pressedElement) {
-			console.log("this", this);
+			console.log(this.mousePosition.x, this.mousePosition.y);
+			this.clonedElementContainer.style.top = this.mousePosition.y + "px";
+			this.clonedElementContainer.style.left = this.mousePosition.x + "px";
 		}
 	}
 
@@ -33,15 +45,23 @@
 		sortableElements.forEach((element) => {
 			element.addEventListener("mousedown", (event) => {
 				this.pressedElement = element;
-				console.log("mousedown on ", this);
+				this.clonedElement = element.cloneNode(true);
+				this.clonedElement.style.cssText = document.defaultView.getComputedStyle(element, "").cssText;
+				removeAllChildren(this.clonedElementContainer);
+				this.clonedElementContainer.appendChild(this.clonedElement);
 			})
 		})
 		window.addEventListener("mouseup", (event) => {
 			this.pressedElement = null;
-			console.log("mouseup", this);
 		})
 	}
 
 	window.sortableObj = sortableObj;
 	window.sortable = sortable;
+
+	function removeAllChildren(element){
+	  while(element.firstChild){
+	    element.removeChild(element.firstChild);
+	  }
+	}
 })();
